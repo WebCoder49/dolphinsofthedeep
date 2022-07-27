5f27d8e6891dc2e733411e0e16a3c4aa09d4e0e9
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2022-07-27 07:27:18 +0100

    Start adding rideability

--- src/main/java/io/github/webcoder49/dolphinsofthedeep/entities/dolphins/DolphinEntity.java
+++ src/main/java/io/github/webcoder49/dolphinsofthedeep/entities/dolphins/DolphinEntity.java
@@ -1,21 +1,30 @@
 package io.github.webcoder49.dolphinsofthedeep.entities.dolphins;
 
 import io.github.webcoder49.dolphinsofthedeep.DolphinsOfTheDeep;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.Tameable;
+import net.minecraft.entity.*;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.ServerConfigHandler;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.Level;
 import org.jetbrains.annotations.Nullable;
@@ -26,18 +35,67 @@ import java.util.UUID;
 /**
  * Extends vanilla dolphin to add vanilla functionality; extra modded functionality added here
  */
-public class DolphinEntity extends net.minecraft.entity.passive.DolphinEntity implements Tameable {
+public class DolphinEntity extends net.minecraft.entity.passive.DolphinEntity implements Tameable, Saddleable {
+    // Components
+    private final SaddledComponent saddledComponent;
+
     /**
      * Constructor for a dolphin
      */
     public DolphinEntity(EntityType<? extends net.minecraft.entity.passive.DolphinEntity> entityType, World world) {
         super(entityType, world);
+        this.saddledComponent = new SaddledComponent(this.dataTracker, BOOST_TIME, SADDLED);
     }
 
-    private static Ingredient TAMING_INGREDIENT;
+    /**
+     * Initialise tracked data
+     */
+    protected void initDataTracker() {
+        super.initDataTracker();
+        // Taming
+        this.dataTracker.startTracking(TAMEABLE_FLAGS, (byte)0);
+        this.dataTracker.startTracking(OWNER_UUID, Optional.empty());
+        // Saddling
+        this.dataTracker.startTracking(SADDLED, false);
+        this.dataTracker.startTracking(BOOST_TIME, 0);
+    }
 
-    /* Tamable */
-    // Attach owner data to DataTracker
+    // NBTs
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+
+        // Taming
+        if (this.getOwnerUuid() != null) { // Tamed
+            // Add Owner UUID
+            nbt.putUuid("Owner", this.getOwnerUuid());
+        }
+        // Saddling
+        this.saddledComponent.writeNbt(nbt);
+    }
+
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        // Get UUID of owner
+        UUID uUID;
+        if (nbt.containsUuid("Owner")) { // UUID
+            uUID = nbt.getUuid("Owner");
+        } else { // Username
+            String string = nbt.getString("Owner");
+            uUID = ServerConfigHandler.getPlayerUuidByName(this.getServer(), string);
+        }
+
+        if (uUID != null) { // Tamed
+            try {
+                this.setOwnerUuid(uUID);
+                this.setTamed(true);
+            } catch (Throwable var4) { // UUID Doesn't exist
+                this.setTamed(false);
+            }
+        }
+        // Saddling
+        this.saddledComponent.readNbt(nbt);
+    }
+
     /** Each bit is 1 flag:
      * 128=
      * 64=
@@ -50,18 +108,22 @@ public class DolphinEntity extends net.minecraft.entity.passive.DolphinEntity im
      */
     protected static final TrackedData<Byte> TAMEABLE_FLAGS;
     protected static final TrackedData<Optional<UUID>> OWNER_UUID;
+    private static Ingredient TAMING_INGREDIENT;
+
+    private static final TrackedData<Boolean> SADDLED;
+    private static final TrackedData<Integer> BOOST_TIME;
+
+    /* Tamable */
+    // Attach owner data to DataTracker
     static {
         TAMEABLE_FLAGS = DataTracker.registerData(DolphinEntity.class, TrackedDataHandlerRegistry.BYTE);
         OWNER_UUID = DataTracker.registerData(DolphinEntity.class, TrackedDataHandlerRegistry.OPTIONAL_UUID);
-    }
 
-    /**
-     * Initialise tracked data
-     */
-    protected void initDataTracker() {
-        super.initDataTracker();
-        this.dataTracker.startTracking(TAMEABLE_FLAGS, (byte)0);
-        this.dataTracker.startTracking(OWNER_UUID, Optional.empty());
+
+        /**
+         * Items used to tame a dolphin = Tropical Fish, Salmon, Cod
+         */
+        TAMING_INGREDIENT = Ingredient.ofItems(Items.TROPICAL_FISH, Items.SALMON, Items.COD);
     }
 
     // Getting and setting owner - lowest-level>highest-level
@@ -78,6 +140,14 @@ public class DolphinEntity extends net.minecraft.entity.passive.DolphinEntity im
         } else {
             this.dataTracker.set(TAMEABLE_FLAGS, (byte)(b & -5)); // Set bit4 to 0
         }
+    }
+
+    /**
+     * Is this dolphin tamed?
+     * @return
+     */
+    public boolean getTamed() {
+        return (this.dataTracker.get(TAMEABLE_FLAGS) & 4) != 0; // Bit 4
     }
 
     /**
@@ -121,52 +191,55 @@ public class DolphinEntity extends net.minecraft.entity.passive.DolphinEntity im
         }
     }
 
-    public void writeCustomDataToNbt(NbtCompound nbt) {
-        super.writeCustomDataToNbt(nbt);
-        if (this.getOwnerUuid() != null) { // Tamed
-            // Add Owner UUID
-            nbt.putUuid("Owner", this.getOwnerUuid());
-        }
-    }
-
-    public void readCustomDataFromNbt(NbtCompound nbt) {
-        super.readCustomDataFromNbt(nbt);
-        // Get UUID of owner
-        UUID uUID;
-        if (nbt.containsUuid("Owner")) { // UUID
-            uUID = nbt.getUuid("Owner");
-        } else { // Username
-            String string = nbt.getString("Owner");
-            uUID = ServerConfigHandler.getPlayerUuidByName(this.getServer(), string);
-        }
-
-        if (uUID != null) { // Tamed
-            try {
-                this.setOwnerUuid(uUID);
-                this.setTamed(true);
-            } catch (Throwable var4) { // UUID Doesn't exist
-                this.setTamed(false);
-            }
-        }
-    }
     // Tame with fish
-    /**
-     * TEST
-     */
     @Override
     public ActionResult interactMob(PlayerEntity player, Hand hand) {
         DolphinsOfTheDeep.log(Level.INFO, "Interacted.");
         // Get item in hand
         ItemStack itemStack = player.getStackInHand(hand);
-        if (this.isTamingItem(itemStack)) {
-            DolphinsOfTheDeep.log(Level.WARN, "Not taming item.");
-            return ActionResult.FAIL;
+        Item item = itemStack.getItem();
+        if (this.world.isClient) {
+            // Just display to player; not whole server
+            if(this.getOwner() == null && this.isTamingItem(itemStack)) {
+                return ActionResult.SUCCESS;
+            } else {
+                return ActionResult.FAIL;
+            }
         } else {
-            DolphinsOfTheDeep.log(Level.INFO, "Taming.");
-            this.setOwner(player);
-            player.sendMessage(Text.translatable(String.format("entity.%1$s.dolphin.tamed", DolphinsOfTheDeep.MOD_ID)));
-            return ActionResult.CONSUME;
+            // TODO: Remove duplicates
+            if(this.getOwner() == null) { // Untamed - try to tame
+                if (item.isFood() && this.isTamingItem(itemStack)) {
+                    DolphinsOfTheDeep.log(Level.INFO, "Taming.");
+                    this.setOwner(player);
+                    this.tellOwner(this.getTranslatedText("tamed"));
+
+                    this.playSound(SoundEvents.ENTITY_DOLPHIN_EAT, 1.0F, 1.0F);
+                    this.eat(itemStack, player, hand);
+
+                    return ActionResult.CONSUME;
+                } else {
+                    DolphinsOfTheDeep.log(Level.WARN, "Not taming item.");
+                    return ActionResult.FAIL;
+                }
+            } else if(this.getOwner() == player) { // Tamed by player.
+                this.tellOwner(Text.of("Hello!"));
+                return ActionResult.SUCCESS;
+            } else { // Tamed by someone else
+                return ActionResult.FAIL;
+            }
         }
+    }
+
+    /**
+     * Get the translated text by path key under this mob.
+     * @param path "."-separated path of key after entity.modid.entityid
+     * @return Translated MutableText
+     */
+    public MutableText getTranslatedText(String path, Object... args) {
+        return Text.translatable(
+                String.format("%1$s.%2$s", this.getType().getTranslationKey(), path),
+                args
+        );
     }
 
     /**
@@ -175,19 +248,61 @@ public class DolphinEntity extends net.minecraft.entity.passive.DolphinEntity im
     public boolean isTamingItem(ItemStack stack) {
         return TAMING_INGREDIENT.test(stack);
     }
-    static {
-        /**
-         * Items used to tame a dolphin = Tropical Fish, Salmon, Cod
-          */
-        TAMING_INGREDIENT = Ingredient.ofItems(Items.TROPICAL_FISH, Items.SALMON, Items.COD);
+
+    /**
+     * Eat an item from the player and use it up.
+     * @param itemStack The ItemStack of food.
+     * @param player The player who is holding the food.
+     * @param hand The player's hand which the food is in.
+     */
+    public void eat(ItemStack itemStack, PlayerEntity player, Hand hand) {
+        this.playSound(SoundEvents.ENTITY_DOLPHIN_EAT, 1F, 1F);
+        if(!player.getAbilities().creativeMode) { // Not creative - use up
+            itemStack.decrement(1); // Use up 1 item
+        }
     }
 
     /* Conversations */
-//    public boolean tellOwner(Text message) {
-//        LivingEntity owner = this.getOwner();
-//        if(owner instanceof PlayerEntity) {
-//            owner.sendMessage(message);
-//        }
-//        return false; // Not player owner
-//    }
+    public boolean tellOwner(Text message) {
+        DolphinsOfTheDeep.log(Level.INFO, message.getString());
+        LivingEntity owner = this.getOwner();
+        if(owner instanceof PlayerEntity) {
+            MutableText chatMessage = Text.empty().copy();
+            chatMessage.append(this.getTranslatedText("chatPrefix", this.getName())
+                    .setStyle(Style.EMPTY.withColor(TextColor.parse("aqua"))));
+            chatMessage.append(message);
+
+            owner.sendMessage(chatMessage);
+            return true;
+        }
+        return false; // Not player owner
+    }
+
+    /* Riding */
+    // Saddleable
+    static {
+        SADDLED = DataTracker.registerData(DolphinEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+        BOOST_TIME = DataTracker.registerData(DolphinEntity.class, TrackedDataHandlerRegistry.INTEGER);
+    }
+    public void onTrackedDataSet(TrackedData<?> data) {
+        if (BOOST_TIME.equals(data) && this.world.isClient) {
+            // Looking at boost time - want to boost now
+            this.saddledComponent.boost();
+        }
+        super.onTrackedDataSet(data);
+    }
+    public boolean isSaddled() {
+        return this.saddledComponent.isSaddled();
+    }
+    public void saddle(@Nullable SoundCategory soundCategory) {
+        this.saddledComponent.setSaddled(true);
+        if(soundCategory != null) { // Play sound
+            this.world.playSoundFromEntity(null, this, SoundEvents.ENTITY_DOLPHIN_SPLASH, soundCategory, 1.0F, 1.0F);
+        }
+    }
+
+    public boolean canBeSaddled() {
+        return this.getTamed();
+    }
+
 }
